2f7cb83cec4f8de010e54863d924034c8ea8418e
MEOW <siyulu111@gmail.com>
2023-04-28 01:50:53 +0800

    尝试连接 openai与 github

---
+++ test001.py
@@ -0,0 +1,15 @@
+import openai
+
+openai.api_key = api_key
+
+def generate_text(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    return response.choices[0].text.strip()
